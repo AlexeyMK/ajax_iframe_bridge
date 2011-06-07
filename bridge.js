8bf85554b128aa1099a468b461b0e78fed6062d3
Eric Allen <ericpallen@gmail.com>
2011-06-07 16:24:59 -0700

    need to have a ready callback

--- bridge.js
+++ bridge.js
@@ -1,11 +1,17 @@
 Bridge = {
   dispatchTable : {},
   nextCallbackId : 0,
-  init : function(url, origin) {
+  init : function(url, origin, callback) {
     //sample origin: https://api.example.com
     Bridge.origin = origin;
+    if(callback === undefined){
+      Bridge.ready = function(){}
+    } else {
+      Bridge.ready = callback;
+    }
     Bridge.iframe = document.createElement('iframe');
     Bridge.iframe.setAttribute('src', url);
+    Bridge.iframe.setAttribute('onload', 'Bridge.ready()');
     document.body.appendChild(Bridge.iframe);
   },
   ajax : function(args) {
@@ -19,7 +25,7 @@ Bridge = {
       args:       args,
       callbackId: callbackId
     };
-    
+
     Bridge.iframe.contentWindow.postMessage(
       JSON.stringify(message), Bridge.origin);
   },
